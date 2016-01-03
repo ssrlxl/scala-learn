ef627eb84f225c9ef944bd87dd5f5f0831f02680
邵盛荣 <shaoshengrong@shaoshengrongdeMacBook-Pro.local>
2016-01-03 12:28:17 +0800

    最大公约数

---
+++ help_ctor.scala
@@ -0,0 +1,40 @@
+
+/*
+	scala 构造器
+	scala private 
+*/
+
+class Rational(n: Int, d: Int){
+
+	require(d != 0)
+
+	private val g = gcd(n.abs, d.abs)
+	val number: Int = n / g
+	val denom: Int = d / g
+
+	def this(n: Int) = this(n, 1)
+
+	override def toString = number +"/" +denom
+
+
+	def add(that: Rational): Rational = 
+		new Rational(
+				number * that.denom + that.number *denom,
+				denom * that.denom
+			)
+
+	private def gcd(a: Int, b: Int):Int = 
+		if (b == 0) a else gcd(b, a %b)		
+}
+
+
+val rat1 = new Rational(10,1)
+var rat2 = new Rational(2)
+println(rat1)
+println(rat2)
+
+var gcd = new Rational(66,42)
+
+println(rat1.add(rat2))
+
+println("最大公约数 ", gcd)
